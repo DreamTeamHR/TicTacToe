2c7f48d469371db812d52e76ebbb593b4efafec9
jodiserla <jodis.erla@gmail.com>
2017-10-27 19:59:03 +0000

    implemented TicTacToe.java along with testcases

--- src/test/java/is/ru/tictactoe/TicTacToeTest.java
+++ src/test/java/is/ru/tictactoe/TicTacToeTest.java
@@ -5,4 +5,26 @@ import org.junit.Test;
 
 public class TicTacToeTest {
 
+	@Test
+	public void testChangePlayer() { 
+		Player p = new Player();
+		TicTacToe t = new TicTacToe();
+		t.changePlayer('X',p);
+		t.changePlayer('O',p);
+		t.changePlayer('X',p);
+		t.changePlayer('O',p);
+
+		assertEquals('O', t.changePlayer(p.getPlayer1(), p));
+	}
+	@Test
+	public void testIsWinner() {
+		TicTacToe t = new TicTacToe();
+		assertEquals(false, t.isWinner());
+	}
+	@Test
+	public void testIsDraw() {
+		TicTacToe t = new TicTacToe();
+		assertEquals(false, t.isDraw());
+	}
+
 }
