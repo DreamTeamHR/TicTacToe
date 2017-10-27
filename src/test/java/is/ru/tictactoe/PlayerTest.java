7d79179855014609438759d842cab9c92d638c62
irisdoggs <iriss16@ru.is>
2017-10-27 15:45:31 +0000

    double checking if player2 test works

--- src/test/java/is/ru/tictactoe/PlayerTest.java
+++ src/test/java/is/ru/tictactoe/PlayerTest.java
@@ -9,16 +9,16 @@ public class PlayerTest {
 	@Test
 	public void testPlayer1(){
 		Player p = new Player();
-		p.setPlayer1('R');
-		assertEquals('R',
+		p.setPlayer1('i');
+		assertEquals('i',
 		p.getPlayer1());
 	}
 
 	@Test
 	public void testPlayer2(){
 		Player p = new Player();
-		p.setPlayer2('L');
-		assertEquals('L',
+		p.setPlayer2('j');
+		assertEquals('j',
 		p.getPlayer2());
 	}
 
